b4c5d951c979d5d90c2e002e60b7398f12fc7eb8
Reza <46368620+rezyious@users.noreply.github.com>
2025-07-26 14:10:46 +0330

    fonts added

--- nvim/.config/nvim/lua/plugins/themes.lua
+++ nvim/.config/nvim/lua/plugins/themes.lua
@@ -1,5 +1,14 @@
 return {
   {
+    "Mofiqul/adwaita.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.adwaita_transparent = true
+      vim.g.adwaita_darker = true
+    end,
+  },
+  {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
