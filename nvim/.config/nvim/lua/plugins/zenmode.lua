436179b6501fa7e270b9e1a74876daff59e20b0a
Reza <46368620+rezyious@users.noreply.github.com>
2025-07-28 06:44:57 +0330

    fix zen

--- nvim/.config/nvim/lua/plugins/zenmode.lua
+++ nvim/.config/nvim/lua/plugins/zenmode.lua
@@ -1,9 +1,7 @@
 return {
   "folke/zen-mode.nvim",
-  config = function()
-    require("zen-mode").toggle({
-      width = 120,
-      backdrop = 0.9,
-    })
-  end,
+  opts = {
+    width = 120,
+    backdrop = 0.9,
+  },
 }
