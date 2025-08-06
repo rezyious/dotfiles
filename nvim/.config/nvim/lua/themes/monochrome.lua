bb23f2ee6369c79127fc37e1173b64672b7e2536
Reza <46368620+rezyious@users.noreply.github.com>
2025-08-06 18:04:40 +0330

    minor changes

--- nvim/.config/nvim/lua/themes/monochrome.lua
+++ nvim/.config/nvim/lua/themes/monochrome.lua
@@ -1,6 +1,7 @@
 return {
   "rezyious/monochrome.nvim",
+  -- dir = "~/git/monochrome.nvim", -- local
   config = function()
-    vim.g.monochrome_style = "amplified"
+    vim.g.monochrome_style = "coolgray"
   end,
 }
