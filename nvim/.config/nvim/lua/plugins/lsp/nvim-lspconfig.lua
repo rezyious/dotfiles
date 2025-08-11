96178bdaed332f00b7148df24a9e568c6ae5e37a
Reza <46368620+rezyious@users.noreply.github.com>
2025-08-11 11:19:51 +0330

    disable lsp

--- nvim/.config/nvim/lua/plugins/lsp/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lsp/nvim-lspconfig.lua
@@ -7,46 +7,55 @@ return {
     -- C
     lspconfig.clangd.setup({
       capabilities = capabilities,
+      autostart = false,
     })
 
     -- python
     lspconfig.pylsp.setup({
+      autostart = false,
       capabilities = capabilities,
     })
 
     -- go
     lspconfig.gopls.setup({
+      autostart = false,
       capabilities = capabilities,
     })
 
     -- lua
     lspconfig.lua_ls.setup({
+      autostart = false,
       capabilities = capabilities,
     })
 
     -- typescript
     lspconfig.ts_ls.setup({
+      autostart = false,
       capabilities = capabilities,
     })
 
     -- css
     lspconfig.cssls.setup({
+      autostart = false,
       capabilities = capabilities,
     })
 
     -- emmet
     lspconfig.emmet_ls.setup({
+      autostart = false,
       capabilities = capabilities,
     })
 
     -- bash
     lspconfig.bashls.setup({
+      autostart = false,
       capabilities = capabilities,
     })
 
     -- Rust
     lspconfig.rust_analyzer.setup({
       capabilities = capabilities,
+      autostart = false,
       settings = {
         ["rust-analyzer"] = {
           standalone = true,
