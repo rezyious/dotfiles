a2dca81f8111d6a6bfdaf2c0a67c0b31463a7ebc
rezyious <46368620+rezyious@users.noreply.github.com>
2025-09-11 03:20:50 +0330

    added neogit

---
+++ nvim/.config/nvim/lua/plugins/neogit.lua
@@ -0,0 +1,11 @@
+return {
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "sindrets/diffview.nvim",
+  },
+  config = function()
+    local neogit = require("neogit")
+    neogit.setup({})
+  end,
+}
