ac8f1ff46afd6230bc12594a57bae1eb8aa42538
rezyious <46368620+rezyious@users.noreply.github.com>
2025-12-26 20:16:30 +0330

    change theme and colors nvim

--- nvim/.config/nvim/lua/themes/onedarkpro.lua
+++ nvim/.config/nvim/lua/themes/onedarkpro.lua
@@ -1,4 +1,34 @@
 return {
     "olimorris/onedarkpro.nvim",
-    priority = 1000, 
+    priority = 1000,
+    config = function()
+        require("onedarkpro").setup({
+            styles = {
+                types = "bold",
+                methods = "bold",
+                keywords = "bold,italic",
+                operators = "bold",
+                variables = "bold,italic",
+            },
+            highlights = {
+                Comment = { fg = "#ffffff" },
+                String = { fg = "#ffffff" },
+                Variable = { fg = "#ffffff", italic = true },
+                Function = { fg = "#61afef", bold = true },
+
+                -- Treesitter
+                ["@string"] = { fg = "#ffffff" },
+                ["@variable"] = { fg = "#ffffff" },
+                ["@property"] = { fg = "#ffffff" },
+                ["@punctuation.delimiter"] = { fg = "#ffffff" },
+                ["@punctuation.bracket"] = { fg = "#ffffff" },
+                ["@punctuation.special"] = { fg = "#ffffff" },
+                ["@function"] = { fg = "#61afef" },
+                ["@keyword"] = { fg = "#c3e88d" },
+            },
+            options = {
+                transparency = false,
+            },
+        })
+    end,
 }
