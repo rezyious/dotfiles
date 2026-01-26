70050c0b93edfa0db8d65d8f50811cfb40fc41ce
rezyious <46368620+rezyious@users.noreply.github.com>
2026-01-26 06:32:27 +0330

    rev 4

--- nvim/.config/nvim/lua/plugins/lsp/mason-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lsp/mason-lspconfig.lua
@@ -1,10 +1,10 @@
 return {
-  "williamboman/mason-lspconfig.nvim",
-  config = function()
-    require("mason-lspconfig").setup({
-      ensure_installed = { "emmet_ls", "ts_ls" , "rust_analyzer" , "bashls" , "clangd" },
-      auto_install = false,
-      automatic_enable = false,
-    })
-  end,
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+        require("mason-lspconfig").setup({
+            ensure_installed = { "emmet_ls", "ts_ls", "rust_analyzer", "bashls", "clangd", "pylsp" },
+            auto_install = false,
+            automatic_enable = false,
+        })
+    end,
 }
