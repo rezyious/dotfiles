c30c3427284becd79be1252d2b1e098c4a542874
rezyious <46368620+rezyious@users.noreply.github.com>
2026-01-31 15:10:11 +0330

    todo comments change

--- nvim/.config/nvim/lua/plugins/todo.lua
+++ nvim/.config/nvim/lua/plugins/todo.lua
@@ -1,12 +1,13 @@
 return {
-  "folke/todo-comments.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  opts = {
-    keywords = {
-      FIX = { icon = " ", color = "#DC2626" },
-      ERROR = { icon = " ", color = "#DC2626" },
-      NOTE = { icon = " ", color = "#00f123" },
-      _ = { icon = " ", color = "#DC2626" },
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+        keywords = {
+            FIX = { icon = " ", color = "#DC2626" },
+            ERROR = { icon = " ", color = "#DC2626" },
+            NOTE = { icon = " ", color = "#00f123" },
+            _ = { icon = " ", color = "#DC2626" },
+            __ = { icon = " ", color = "#00f123" },
+        },
     },
-  },
 }
